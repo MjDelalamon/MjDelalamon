3fb143ce3ab6cb72a5ad5e77976adb2d20f9948e
Gemma Mendoza <gbm@192.168.1.18>
2025-08-07 22:37:04 +0800

    new

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
-// vite.config.ts
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/MjDelalamon/', // MUST match your repo name
+  base: '/MjDelalamon/', // <- this is critical!
   plugins: [react()],
 });
