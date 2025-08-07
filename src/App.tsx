dc3fad710895893b1fe157ad5bfdf7550d1fd376
Gemma Mendoza <gbm@192.168.1.18>
2025-08-07 21:25:45 +0800

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,41 @@
+import { Routes, Route, Navigate } from "react-router-dom";
+
+import Sidebar from "./components/SideBar";
+import Intro from "./Intro";
+import Staff from "./StaffSide";
+import Owner from "./OwnerSide";
+import Dashboard from "./AdminPage/Dashboard";
+import Customer from "./AdminPage/Customer";
+import Orders from "./AdminPage/Orders";
+import Rewards from "./AdminPage/Reward";
+import Menu from "./AdminPage/Menu";
+
+import "./Style/Dashboard.css";
+import "./Style/App.css";
+import "./Style/index.css";
+import "./Style/intro.css";
+import "./Style/sideBar.css";
+import "./Style/Customer.css";
+import "./Style/Order.css";
+import "./Style/Reward.css";
+import "./Style/Menu.css";
+
+function App() {
+  return (
+    <Routes>
+      {/* Default path redirects to /Intro */}
+      <Route path="/" element={<Navigate to="/Intro" replace />} />
+      <Route path="/Intro" element={<Intro />} />
+      <Route path="/Staff" element={<Staff />} />
+      <Route path="/Owner" element={<Owner />} />
+      <Route path="/Sidebar" element={<Sidebar />} />
+      <Route path="/Dashboard" element={<Dashboard />} />
+      <Route path="/customers" element={<Customer />} />
+      <Route path="/orders" element={<Orders />} />
+      <Route path="/rewards" element={<Rewards />} />
+      <Route path="/menu" element={<Menu />} />
+    </Routes>
+  );
+}
+
+export default App;
